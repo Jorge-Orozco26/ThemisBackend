68c3a608cb4bf84fbf0d58538a1f59a1cd34a549
Jorge Orozco <creneorozco@gmail.com>
2023-10-21 00:12:32 -0600

    tabla rol agregada en cuentas

--- cuentas/models.py
+++ cuentas/models.py
@@ -2,13 +2,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+class Rol(models.Model):
+    ID_Rol = models.AutoField(primary_key=True)
+    Codigo_Rol = models.CharField(max_length=50)
+    Nombre_Rol = models.CharField(max_length=255)
+    Usuario_Registro = models.CharField(max_length=50)
+    Fecha_Registro = models.DateTimeField()
+    estado = models.BooleanField()
+
+    def __str__(self):
+        return self.Nombre_Rol
+
 class CustomUser(AbstractUser):  #hereda la clase de abstractUser de Django y la extiende   
     email = models.EmailField(unique=True)
     ID_Rol = models.IntegerField()
     Usuario_Registro = models.CharField(max_length=50)
     Fecha_Registro = models.DateTimeField()
 
+    rol= models.ForeignKey(Rol, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.username
+
 
