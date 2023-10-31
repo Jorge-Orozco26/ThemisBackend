4f922aeffb84bf56e2c509b9cee7171ef204c541
Jorge Orozco <creneorozco@gmail.com>
2023-10-30 22:40:20 -0600

    correccion en get tramites procedimientos

--- tramitesLeg/urls.py
+++ tramitesLeg/urls.py
@@ -2,8 +2,12 @@ from django.urls import path, include
 from rest_framework import routers
 from tramitesLeg import views
 from .views import procedimientolegal
+from .views import PlantillaSoli
 
 
 urlpatterns = [
-    path('procedimientolegal/', procedimientolegal.as_view(), name='procedimientolegal_list')
+    path('procedimientolegal/', procedimientolegal.as_view(), name='procedimientolegal_list'),
+    path('procedimientolegal/<int:id>', procedimientolegal.as_view(), name='procedimientolegal_process'),
+    path('plantillasolicitud/', PlantillaSoli.as_view(), name='plantillasolicitud_list'),
+    path('plantillasolicitud/<int:id>', PlantillaSoli.as_view(), name='plantillasolicitud_process')
 ]
--- tramitesLeg/views.py
+++ tramitesLeg/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from django.views import View
 from .models import Procedimientos_Legales
+from .models import Plantilla_Solicitud
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -17,7 +18,8 @@ class procedimientolegal(View):
 
     def get(self, request, id=0):
         if (id>0):
-            procedimientos= list(Procedimientos_Legales.objects.filter(id=id).values()) #listar los registros por id
+            
+            procedimiento = Procedimientos_Legales.objects.get(ID_Procedimientos_Legales=id)#listar los registros por id
             if len(procedimientos)> 0:
                 Procedure =procedimientos[0]
                 datos = {'message': "Transacción exitosa", 'Procedure': Procedure }
@@ -31,7 +33,7 @@ class procedimientolegal(View):
             else:
                 datos = {'message': "Transacción inválida..."}
             return JsonResponse(datos)
-
+        
 
 
 #EL METODO POST NO FUNCIONA PORQUE DEPENDE DE UN CLAVE: REGISTRO_USUARIO/FOREIGN_KEY
@@ -57,3 +59,49 @@ class procedimientolegal(View):
         pass
     def delete(self,request):
         pass
+
+
+                                    ####CRUD MODELO PLANTILLA SOLICITUD
+
+class PlantillaSoli(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs): #
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        if (id>0):
+            plantilla= list(Plantilla_Solicitud.objects.filter(id=id).values())
+            if len(plantilla)> 0:
+                plant =plant[0]
+                datos = {'message': "Transacción exitosa", 'plant': plant }
+            else:
+                datos = {'message': "Transacción inválida..."}
+            return JsonResponse(datos)
+        else:
+            plantilla = list(Plantilla_Solicitud.objects.values())
+            if len(plantilla)>0:
+                datos = {'message': "Transacción exitosa", 'plantilla': plantilla }
+            else:
+                datos = {'message': "Transacción inválida..."}
+            return JsonResponse(datos)
+    
+
+    def post(self,request):
+        try:
+            jd = json.loads(request.body)
+            Plantilla_Solicitud.objects.create(
+                Nombre=jd['Nombre'],
+                Contenido=jd['Contenido'],    
+                Usuario_Registro=jd['Usuario_Registro'],
+                Fecha_Registro=jd['Fecha_Registro'],
+                Estado=jd['Estado'],
+                procedimiento_legales_id=jd['procedimiento_legales_id'],
+                usuario_creador_id=jd['usuario_creador_id']
+            )
+            datos = {'message': "Transacción exitosa"}
+            return JsonResponse(datos)
+        except KeyError as e:
+            # Manejar el caso en el que una clave está ausente en la solicitud POST
+            datos = {'error': f'La clave {str(e)} no se encuentra en la solicitud POST.'}
+            return JsonResponse(datos, status=400)
